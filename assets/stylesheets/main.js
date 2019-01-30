9fe8b53b7db399d1531e159bb693ef8e048d5bcf
Meghal11 <meghal.singhal@gmail.com>
2019-01-30 20:31:19 +0530

    Update main.js

--- assets/stylesheets/main.js
+++ assets/stylesheets/main.js
@@ -598,6 +598,7 @@ function hidePlaylist() {
 
 function performSearch() {
     event.preventDefault();
+        
     searched = true;
     if (searching == 1) {
         ul = document.getElementById("searchlist");
